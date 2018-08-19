ac21b3b45506e971bf8c229611a890397f469851
Jim Rybarski <jim@rybarski.com>
2018-08-19 10:33:44 -0500

    update test line in benchmarks

--- benches/mod.rs
+++ benches/mod.rs
@@ -8,7 +8,7 @@ mod tests {
 
     #[bench]
     fn bench_parse_sequence_identifier(b: &mut Bencher) {
-        let sequence_identifier = "@M03745:11:000000000-B54L5:1:2108:4127:8949";
+        let sequence_identifier = "@M03745:11:000000000-B54L5:1:2108:4127:8949 1:N:0:0";
         b.iter(|| illumina_coordinates::parse_sequence_identifier(&sequence_identifier).unwrap());
     }
 }
